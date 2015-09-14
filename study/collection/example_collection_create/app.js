a857b6cd9130e4975c089683ac1e3170bc9926be
guosheng <cff20@sina.com>
2015-09-14 22:09:13 +0800

    up
    up

---
+++ study/collection/example_collection_create/app.js
@@ -0,0 +1,19 @@
+var InvoiceItemModel = Backbone.Model.extend({});
+
+var InvoiceItemCollection = Backbone.Collection.extend({
+    model: InvoiceItemModel
+});
+
+//创建集合
+var invoiceItemCollection = new InvoiceItemModel([{
+    name: 'kyo',
+    age: 18
+}, {
+    name: 'lily',
+    age: 15
+}, {
+    name: 'jone',
+    age: 19
+}]);
+
+console.log(invoiceItemCollection);
