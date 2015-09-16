6102050c09fd347e7990eef619abefc7be380390
guosheng <cff20@sina.com>
2015-09-16 21:55:09 +0800

    push pop unshift shift

---
+++ study/collection/collection_pop/app.js
@@ -0,0 +1,54 @@
+var InvoiceItemModel = Backbone.Model.extend({});
+
+var InvoiceItemCollection = Backbone.Collection.extend({
+    model: InvoiceItemModel
+});
+
+//创建集合，并指定id
+var invoiceItemCollection = new InvoiceItemCollection([{
+    id: 'cc1',
+    name: 'kyo',
+    age: 18
+}, {
+    id: 'cc2',
+    name: 'lily',
+    age: 15
+}, {
+    id: 'cc3',
+    name: 'jone',
+    age: 19
+}]);
+
+//使用push加在末尾
+invoiceItemCollection.push({
+    id: 'cc4',
+    name: 'kit',
+    age: 22
+});
+
+console.log(invoiceItemCollection);
+
+//使用pop从末尾取值
+var oneModel = invoiceItemCollection.pop();
+console.log(oneModel);
+
+//末尾的model已经删除了
+console.log(invoiceItemCollection);
+
+//push是加末尾，pop是取末尾
+//如果要加最前面，用unshift(model)，移除最前面用shift();
+
+invoiceItemCollection.unshift({
+    id: 'cc5',
+    name: '555',
+    age: 23
+});
+console.log(invoiceItemCollection);
+
+oneModel = invoiceItemCollection.shift();
+console.log(oneModel);
+
+
+//如果用队列。
+//末尾加push(model)，取最前面shift()
+//最前面加unshift(model), 取最后面pop()
