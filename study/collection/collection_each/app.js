6992c496d962287d0ac9c7147c95104d8e624a11
guosheng <guosheng@lamahui.com>
2015-09-18 08:46:38 +0800

    each

---
+++ study/collection/collection_each/app.js
@@ -0,0 +1,47 @@
+var InvoiceItemModel = Backbone.Model.extend({});
+
+var InvoiceItemCollection = Backbone.Collection.extend({
+    model: InvoiceItemModel
+});
+
+//创建集合，并指定id
+var invoiceItemCollection = new InvoiceItemCollection([{
+    id: 'cc1',
+    name: 'kyo',
+    age: 18
+}, {
+    id: 'cc2',
+    name: 'lily',
+    age: 12
+}, {
+    id: 'cc3',
+    name: 'jone',
+    age: 19
+}, {
+    id: 'cc4',
+    name: 'jack',
+    age: 12
+}]);
+
+//each
+invoiceItemCollection.each(function (model, index, list) {
+    console.log(model.get('name'));
+});
+
+//every
+isAllThan18 = invoiceItemCollection.every(function (model, index, list) {
+    return model.get('age') > 18;
+});
+console.log(isAllThan18); //=>false;
+
+//some
+hasSomeThan18 = invoiceItemCollection.some(function (model, index, list) {
+    return model.get('age') > 18;
+});
+console.log(hasSomeThan18); //=>true;
+
+//map
+ages = invoiceItemCollection.map(function (model, index, list) {
+    return model.get('age');
+});
+console.log(ages); //=> [18, 12, 19, 12]
